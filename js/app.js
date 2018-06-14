2974e28cd31579780c6f83cb54624aab536f6087
DolceLemus <dulce.lemus.lara@gmail.com>
2018-06-14 10:35:15 -0500

    desarrollando menu de burger

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 var btnBurger = document.getElementById("btn-burger");
+
+function showMenu(){
+    var menu = document.getElementById("op-menu");
+
+    if(menu.classList.contains("disabled-m")){
+        menu.classList.remove("disabled-m");
+        menu.classList.add("enable-m");
+        menu.classList.add("enable-line");
+    }
+    else {
+        menu.classList.remove("enable-m");
+        menu.classList.add("disabled-m");
+    }
+}
+
+btnBurger.addEventListener('click', showMenu);
